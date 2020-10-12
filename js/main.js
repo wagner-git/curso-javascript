c0fba0a579e78089fc1c0bd94ade3d0baa518f79
wagner <wagner_info300@yahoo.com.br>
2020-10-12 17:50:30 -0300

    Introdução ao javscript

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//alert("Meu primeiro js!");
+/*var nome = "Wagner Alves";
+var idade = 45;
+
+alert(nome + idade);
+
+console.log(nome);
+console.log(idade);*/
+
+/*var lista = ["maçã", "pêra", "laranja"];
+
+console.log(lista);
+console.log(lista.reverse());
+console.log(lista.length);
+console.log(lista.toString());
+lista.push("goiaba");
+console.log(lista);
+lista.pop();//remove o último
+console.log(lista);
+console.log(lista.join(" - "));// separador da lista passa a ser -
+*/
+/*var frutas = [{nome:"Maçã", cor: "Vermelha"}, {nome:"goiaba", cor:"verde"}];
+console.log(frutas);
+console.log(frutas[0].cor);
+console.log(frutas[0].nome);
+*/
+
+/*var idade = prompt("Qual sua idade?");
+if(idade>=18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}*/
+
+/*var count = 0;
+while(count<=5){
+    console.log(count);
+    count++;
+}*/
+/*
+for(var count = 0; count<=5; count++){
+    console.log(count);
+    alert(count);
+}*/
+
+for(var count = 5; count>=0; count--){
+    console.log(count);
+    alert(count);
+}
